a17a434f8cf12745eabae31ce3480769bc06be45
Karla Guadron <ksguadron@gmail.com>
2019-05-25 16:00:17 -0700

    Wrote function to display time

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+$(document).ready(function() {
+  setInterval(function(){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    let time = hours + ':' + minutes + ':' + seconds;
+    $('#clock').html(time);
+}, 1000);
+});
