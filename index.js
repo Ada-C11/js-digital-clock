0fef8ffbe7e94a8de59cf5e65c28371842338cec
Nidhi <nidhixpatel@gmail.com>
2019-05-25 13:04:18 -0700

    added function and set interval to update clock by second

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+console.log("this is a test")
+
+
+ 
+function updateClock() {
+  let clockNow = document.getElementById('clock');
+  let now = new Date(Date.now())
+  let min = now.getMinutes()
+  let hour = now.getHours()
+  let second = now.getSeconds()
+  let clockTime = hour + ":" + min + ":" + second
+  clockNow.innerHTML = clockTime
+}
+
+
+setInterval(updateClock, 1000);
