0fcf3c9685add9ffe2e8ae01fe43aeeeaa2c995d
Nara <naraa_520@yahoo.com>
2019-05-24 19:32:33 -0700

    added clock function

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+const clock = function clock() {
+  let now = new Date();
+  let time = now.toLocaleTimeString();
+  // let hours = now.getHours();
+  // let minutes = now.getMinutes();
+  // let seconds = now.getSeconds();
+  // let time;
+  // if (hours <= 12){
+  //   time = `${hours}:${minutes}:${seconds} AM`
+  // } else {
+  //   time = `${hours%12}:${minutes}:${seconds} PM`
+  // }
+  return time
+}
+
+const showTime = function showTime(){
+  $('#clock').html(clock());
+}
+
+setInterval(showTime, 1000);
+
