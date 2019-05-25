464df15ac41da113b9ca7d8524d38c7687bad039
Nara <naraa_520@yahoo.com>
2019-05-25 12:55:23 -0700

    Added times for different cities

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Your code here
-const clock = function clock() {
-  let now = new Date();
-  let time = now.toLocaleTimeString();
-  // let hours = now.getHours();
-  // let minutes = now.getMinutes();
-  // let seconds = now.getSeconds();
-  // let time;
-  // if (hours <= 12){
-  //   time = `${hours}:${minutes}:${seconds} AM`
-  // } else {
-  //   time = `${hours%12}:${minutes}:${seconds} PM`
-  // }
-  return time
-}
+$(document).ready(function() {
+  const clock = function clock() {
+    let date = new Date();
+    const weekday =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']  
 
-const showTime = function showTime(){
-  $('#clock').html(clock());
-}
+    const calcTime = function calcTime(city, offset) {
+      let localTime = date.getTime();
+      let localOffset = date.getTimezoneOffset() * 60000;
+      let utc = localTime + localOffset;
+      let localDate = new Date(utc + (3600000*offset));
 
-setInterval(showTime, 1000);
+      return  `${city}: ${weekday[localDate.getDay()]}, ${localDate.toLocaleString()}`;
+    }
 
+    $('#clock1').html(calcTime('Seattle', '-7'));
+    $('#clock2').html(calcTime('Paris', '+2'));
+    $('#clock3').html(calcTime('Sydney', '+10'));
+    $('#clock4').html(calcTime('Tokyo', '+9'));  
+  }
+  setInterval(clock, 1000);
+});
