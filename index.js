cdaa88bbf5f3f63a246d5809f50b69eae20a3c35
amyesh <amyesh08@gmail.com>
2019-05-25 18:42:59 -0700

    completed local clock and added styling

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+$(document).ready(function(){
+    function tickTock() {
+        const time = new Date();
+        let h = time.getHours();
+        let m = ((time.getMinutes()<10?'0':'') + time.getMinutes())
+        let s = ((time.getSeconds()<10?'0':'') + time.getSeconds())
+        let clock = `${h}:${m}:${s}`;
+        $("#clock").html(clock);
+    }
+    setInterval(tickTock, 1000);
+});
