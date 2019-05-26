f6293a9a126c806dd340bab28c2389731da02e7b
Carla Bosco <carlabosco@gmail.com>
2019-05-25 18:41:34 -0700

    Clock is displayed and updates each second

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function clock() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const time = `${hours}:${minutes}:${seconds}`;
+  
+  document.getElementById("clock").textContent = time;
+  
+  //call function clock every second
+  setInterval(clock, 1000);
+}
+
+clock();
