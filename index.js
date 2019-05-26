25b61ff4079a7c36be1a134692e7ea262fe8f43a
Jansen <v.jansen.martin@gmail.com>
2019-05-26 15:11:57 -0700

    Initialize jQuery; Display static, current time

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+// let string = "hi";
+// let date = new Date().toLocaleTimeString();
+let date = new Date;
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+
+let time = `${hour}:${minutes}:${seconds}`;
+
+
+$('<p>' + time + '</p>').appendTo('#clock');
