a7eeb71a42a053bd610b5db2f0dc997619b7bfb4
Tatiana Quintana <tatsqui@gmail.com>
2019-05-26 13:03:26 -0700

    adding clock logic

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+let currentTime = new Date();
+
+let currentHours = currentTime.getHours();
+let currentMinutes = currentTime.getMinutes();
+let currentSeconds = currentTime.getSeconds();
+
+currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
