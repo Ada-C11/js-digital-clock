54e843264a52cab0f53dedfdc12ba09f6f2cbf65
Bitaman <b.amani.s@gmail.com>
2019-05-26 20:25:08 -0700

    added digital clock

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+
+const updateClock = function updateClock() {
+     let currentTime = new Date();
+     let londonTime = new Date
+     let currentHoures = currentTime.getHours();
+     let currentMinutes = currentTime.getMinutes();
+     let currentSeconds = currentTime.getSeconds();
+
+     currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+     currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+     let timeOfDay = (currentHoures < 12) ? "AM" : "PM";
+     currentHoures = (currentHoures > 12) ? currentHoures - 12 : currentHoures;
+
+     let currentTimeString = currentHoures + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+      
+    //  let intervalID = scope.setInterval(new Date(), 1000);
+     $('#clock').html(currentTimeString);
+
+    }
+    $(document).ready(function() {
+      setInterval('updateClock()', 1000);
+    })  
+ 
