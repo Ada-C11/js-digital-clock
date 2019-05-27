52292c4fbe8c0fc208f52febeb72cd9924621e19
amyesh <amyesh08@gmail.com>
2019-05-27 14:07:42 -0700

    added multiple timezone clocks

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
-    function tickTock() {
-        let city = 'Seattle'
+    const tickTock = function tickTock() {
         const time = new Date();
         const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const months = [
@@ -18,13 +17,23 @@ $(document).ready(function(){
             'November', 
             'December'
         ];
-    
-        let h = time.getHours();
-        let m = ((time.getMinutes()<10?'0':'') + time.getMinutes())
-        let s = ((time.getSeconds()<10?'0':'') + time.getSeconds())
-        let clock = `${h}:${m}:${s}`;
-        let date = `${weekdays[time.getDay()]}, ${months[time.getMonth()]} ${time.getDate()}, ${time.getFullYear()}`;
-        $("#clock").html(`<p class='city'>${city}:</p><p>${clock}</p><p class='date'>${date}</p>`);
+
+        const timeZone = function timeZone(city, offset) {
+            let localTime = time.getTime();
+            let localOffset = time.getTimezoneOffset() * 60000;
+            let utc = localTime + localOffset;
+            let localDate = new Date(utc + (3600000*offset));
+            let h = localDate.getHours();
+            let m = ((localDate.getMinutes()<10?'0':'') + localDate.getMinutes())
+            let s = ((localDate.getSeconds()<10?'0':'') + localDate.getSeconds())
+            let clock = `${h}:${m}:${s}`;
+            let date = `${weekdays[localDate.getDay()]}, ${months[localDate.getMonth()]} ${localDate.getDate()}, ${localDate.getFullYear()}`;
+            return `<p class='city'>${city}:</p><p>${clock}</p><p class='date'>${date}</p>`;
+        }
+        $('#clock').html(timeZone('Seattle', '-7'));
+        $('#clock2').html(timeZone('Bora Bora', '-10'));
+        $('#clock3').html(timeZone('Kirkenes', '+2'));
+        $('#clock4').html(timeZone('Singapore', '+8')); 
     }
     setInterval(tickTock, 1000);
 });
