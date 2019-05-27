af1a9392adf9621bcc7ca194a868126aa3a4eb2c
rosalyn <rosalyn.anuseh@gmail.com>
2019-05-26 18:33:16 -0700

    project complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+
+$(document).ready(function () {
+  setAllClocks()
+  setInterval(setAllClocks, 1000)
+
+});
+
+function setClock(timeZone, city) {
+  let now = new Date().toLocaleString('en-US', { timeZone });
+
+  $(`#clock-${city}`).html(`${now}`)
+
+}
+
+function setAllClocks() {
+  setClock('America/Los_Angeles', 'seattle')
+  setClock('Africa/Accra', 'accra')
+  setClock('Asia/Tokyo', 'tokyo')
+  setClock('America/Toronto', 'toronto')
+
+}
+
+
+  
+
+
+
+
+
+
+
