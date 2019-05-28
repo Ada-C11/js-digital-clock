123728eecad27262d700f4c398cdd9d2cb9c3ac6
Nidhi <nidhixpatel@gmail.com>
2019-05-28 09:22:13 -0700

    added 0 for times under 10

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Your code here
-console.log("this is a test")
+// console.log("this is a test")
 
 
  
 function updateClock() {
   let clockNow = document.getElementById('clock');
   let now = new Date(Date.now())
-  let min = now.getMinutes()
-  let hour = now.getHours()
-  let second = now.getSeconds()
+  let min = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()
+  let hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours()
+  let second = now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds()
   let clockTime = hour + ":" + min + ":" + second
   clockNow.innerHTML = clockTime
 }
