57009420227cdb96f5d5ec231b77cd54fdd27c8b
Mina Shin <minamishin@hotmail.com>
2019-05-28 00:38:07 -0700

    added am pm notation

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ $(document).ready(function() {
     const hour = current.getHours();
     const minutes = current.getMinutes();
     const seconds = current.getSeconds();
+    const amPm = hour >= 12 ? 'PM' : 'AM';
 
-    return $('#clock').text(`${twelveHour(hour)}:${zero(minutes)}:${zero(seconds)}`);
+    return $('#clock').text(`${twelveHour(hour)}:${zero(minutes)}:${zero(seconds)} ${amPm}`);
     }
     
     const interval = window.setInterval(clock, 500);
