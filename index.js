d463b63f0b769618e6ec12fd03d10c7d77e7ec83
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-28 09:35:34 -0700

    Add .js to display current date & time

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+const padWithZeros = (num) => {
+  if (num < 10) return `0${num}`
+  return num;
+}
+
+$(document).ready(() => {
+  const clock = () => {
+    const rightNow = new Date(Date.now());
+    const dateString = rightNow.toDateString();
+    const formattedDate = `<div>${dateString}</div>`;
+    $('#clock').html(formattedDate);
+
+    const hr = padWithZeros(rightNow.getHours());
+    const min = padWithZeros(rightNow.getMinutes());
+    const sec = padWithZeros(rightNow.getSeconds());
+    const formattedTime = `<div>${hr}:${min}:${sec}</div>`;
+    $('#clock').append(formattedTime);
+  }
+  window.setInterval(clock, 1000);
+});
