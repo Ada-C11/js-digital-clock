f13666a6e6f60949b9fd7b000d197a6b9dac6c84
Mina Shin <minamishin@hotmail.com>
2019-05-27 22:36:00 -0700

    added leading zeros to minutes and seconds

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 $(document).ready(function() { 
-  const clock = () => {
-  let current = new Date();
-  const hour = current.getHours();
-  const minutes = current.getMinutes();
-  const seconds = current.getSeconds();
+    const zero = number => {
+        if (number.toString().length === 1) {
+            number = "0" + number;
+        }
+        return number;
+    }
 
-  return $('#clock').text(`${hour}:${minutes}:${seconds}`);
-  }
-  const interval = window.setInterval(clock, 500);
+    const clock = () => {
+    let current = new Date();
+    const hour = current.getHours();
+    const minutes = current.getMinutes();
+    const seconds = current.getSeconds();
+
+    return $('#clock').text(`${hour}:${zero(minutes)}:${zero(seconds)}`);
+    }
+    
+    const interval = window.setInterval(clock, 500);
       
-  module.exports = index;
+    module.exports = index;
 });
