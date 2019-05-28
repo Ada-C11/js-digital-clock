5d454f1c5b15544aaca544bb06321d79170b18ec
Erica Norris <norrise120@gmail.com>
2019-05-28 13:40:04 -0700

    Added in soooo many semicolons, like a real js programmer

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Your code here
 const findTime = () => { 
     const date = new Date;
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
-    const formattedHours = (hours < 10 ? '0' : '') + hours
-    const formattedMinutes = (minutes < 10 ? '0' : '') + minutes
-    const formattedSeconds = (seconds < 10 ? '0' : '') + seconds
-    const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`
+    const formattedHours = (hours < 10 ? '0' : '') + hours;
+    const formattedMinutes = (minutes < 10 ? '0' : '') + minutes;
+    const formattedSeconds = (seconds < 10 ? '0' : '') + seconds;
+    const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
     document.getElementById('clock').innerHTML = formattedTime;
 }
 
