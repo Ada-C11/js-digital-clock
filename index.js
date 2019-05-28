0bc2a0053bf71e67399986b1f3867b1033d14fcb
Mina Shin <minamishin@hotmail.com>
2019-05-27 22:30:30 -0700

    created initial clock

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+$(document).ready(function() { 
+  const clock = () => {
+  let current = new Date();
+  const hour = current.getHours();
+  const minutes = current.getMinutes();
+  const seconds = current.getSeconds();
+
+  return $('#clock').text(`${hour}:${minutes}:${seconds}`);
+  }
+  const interval = window.setInterval(clock, 500);
+      
+  module.exports = index;
+});
