25b3ef42321699624b9447b1f6b7ceee254c9e02
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 07:18:41 -0700

    wrote setTime method and used jquery to display clock

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+const displayTime = () => {
+    const currTime = new Date(Date.now());
+    const currTimeString = `${currTime.getHours()} : ${currTime.getMinutes()} : ${currTime.getSeconds()}`;
+    $('#clock').html(currTimeString);
+    return currTimeString;
+};
+$(document).ready(function() {
+    setInterval(displayTime, 1000);
+});
