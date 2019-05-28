ee36ab0ce5a91a005d3813790e131e9efeafeef6
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-27 22:40:12 -0700

    wrote function, little bit of styling

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+const clock = () => {
+  let time = new Date();
+  let nowHours = time.getHours();
+  let nowMinutes = time.getMinutes();
+  let nowSeconds = time.getSeconds();
+  let currentTime = `${nowHours}` + ':' + `${nowMinutes}` + ':' + `${nowSeconds}`;
+  document.getElementById("clock").innerHTML = currentTime;
+}
+
+setInterval(clock, 1000);
