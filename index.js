838df68cc1a39d0fff2cd6aadd003c18403bc026
Erica Norris <norrise120@gmail.com>
2019-05-27 21:44:01 -0700

    logic for clock

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const findTime = () => { 
+    const date = new Date;
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+
+    const formattedHours = (hours < 10 ? '0' : '') + hours
+    const formattedMinutes = (minutes < 10 ? '0' : '') + minutes
+    const formattedSeconds = (seconds < 10 ? '0' : '') + seconds
+    const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`
+    document.getElementById('clock').innerHTML = formattedTime;
+}
+
+setInterval(findTime, 1000);
