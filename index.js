2d3344d476a15432db281c3d5a26469cdf8c0cbd
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 14:45:53 -0700

    added buttons to control font, color, alignment, and 12/24 hour display

--- index.js
+++ index.js
@@ -6,11 +6,45 @@ const padNum = (num) => {
 
 const displayTime = () => {
     const currTime = new Date(Date.now());
-    const currTimeString = `${padNum(currTime.getHours())} ` +
+    let hours = currTime.getHours();
+    let timePeriod = (currTime.getHours() > 12) ? " PM": " AM";
+    if ($('#clock').hasClass("12-hour")){
+        hours = padNum(hours % 12);
+    } else {
+        hours = padNum(hours);
+    }
+    let currTimeString = `${hours} ` +
     `: ${padNum(currTime.getMinutes())} : ${padNum(currTime.getSeconds())}`;
+ 
+    if ($('#clock').hasClass("12-hour")) {
+        currTimeString += timePeriod;
+    }
     $('#clock').html(currTimeString);
     return currTimeString;
 };
+
 $(document).ready(function() {
     setInterval(displayTime, 1000);
+    clock = $('#clock');
+    $("#colors").on('click', 'button', function(event) {
+        clock.removeClass("black white yellow");
+        clock.addClass($(this).attr('class'));
+    });
+    $("#alignment").on('click', 'button', function(event) {
+        clock.removeClass("left right center");
+        clock.addClass($(this).attr('class'));
+    });
+    $("#font").on('click', 'button', function(event) {
+        clock.removeClass("monospace serif sans-serif");
+        clock.addClass($(this).attr('class'));
+    });
+    $("#toggle-hours").click( function(event) {
+        if ($(clock).hasClass("12-hour")) {
+            $(clock).removeClass('12-hour');
+            $(this).html("12 Hour Mode");
+        } else {
+            $(clock).addClass('12-hour');
+            $(this).html("24 Hour Mode");
+        }
+    });
 });
