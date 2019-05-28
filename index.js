939a3ec33139451a4766e6a46a4ae337b6fdc989
kateannnichols <kateannnichols@gmail.com>
2019-05-27 20:38:33 -0700

    digital clock

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+
+$(document).ready(function() {
+    const time = () => {
+        const clock = document.getElementById('now');
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let now = `${hours}:${minutes}:${seconds}`;
+        $('#now').html(time);
+        console.log(now);
+        return clock
+    }
+    setInterval(time, 1000);
+});
