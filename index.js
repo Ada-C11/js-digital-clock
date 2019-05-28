b38c2b58b0ea6725fc42c4357a87f2ee74af7e6e
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 07:37:53 -0700

    formatted numbers to be 2 digits

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // Your code here
+const padNum = (num) => {
+    result= (num < 10) ?  '0' + num.toString() : num.toString();
+    return result;
+};
+
 const displayTime = () => {
     const currTime = new Date(Date.now());
-    const currTimeString = `${currTime.getHours()} : ${currTime.getMinutes()} : ${currTime.getSeconds()}`;
+    const currTimeString = `${padNum(currTime.getHours())} ` +
+    `: ${padNum(currTime.getMinutes())} : ${padNum(currTime.getSeconds())}`;
     $('#clock').html(currTimeString);
     return currTimeString;
 };
